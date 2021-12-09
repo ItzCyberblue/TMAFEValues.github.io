2ab80398584e1615ac6a3bcf24d30bd611e538c2
XboxPro39 <57122545+XboxPro39@users.noreply.github.com>
2021-12-09 03:51:14 -0600

    Added Code

---
+++ questions.js
@@ -0,0 +1,502 @@
+questions = [
+    {
+        "question": "Military action only leads to more death and more unwanted consequences.",
+        "effect": {
+            "foreign": 20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Alliances only harm TMAFE and cause further problems.",
+        "effect": {
+            "foreign": -20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Our enemies must face our rath when they cross us.",
+        "effect": {
+            "foreign": -20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Brute military force should always be an option.",
+        "effect": {
+            "foreign": -20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "We should create a universal alliance to combat terror.",
+        "effect": {
+            "foreign": 20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Any isolation of TMAFE will harm it.",
+        "effect": {
+            "foreign": 20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Smaller nations and servers should be TMAFE subjects since they're too weak to govern themselves.",
+        "effect": {
+            "foreign": -20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "A world government would be beneficial.",
+        "effect": {
+            "foreign": 20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Always be agressive toward enemies, no matter what the cost.",
+        "effect": {
+            "foreign": -20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Cooperation of nations leads to eternal peace.",
+        "effect": {
+            "foreign": 20,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Safety is more important than liberty in most or all cases.",
+        "effect": {
+            "foreign": 0,
+            "civil": -20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "The TMAFE government should not restrict what words can be spoken, as all Frontierians are entitled to free speech.",
+        "effect": {
+            "foreign": 0,
+            "civil": 20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Enemy catchphrases and controversial jokes or words should be banned.",
+        "effect": {
+            "foreign": -10,
+            "civil": -20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Free speech is too dangerous to be trusted with the population.",
+        "effect": {
+            "foreign": 0,
+            "civil": -20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "When tyranny becomes law, resistance becomes duty.",
+        "effect": {
+            "foreign": 0,
+            "civil": 20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Freedom is a priceless idea that must be protected by bloodshed if necessary.",
+        "effect": {
+            "foreign": 0,
+            "civil": 20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "The populace makes poor decisions, therefore they must be guided by a strong state.",
+        "effect": {
+            "foreign": 0,
+            "civil": -20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "'We the people' should be feared by all governments.",
+        "effect": {
+            "foreign": 0,
+            "civil": 20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Sometimes the TMAFE government needs to monitor it's people to combat domestic terror.",
+        "effect": {
+            "foreign": 0,
+            "civil": -20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "All authority should be questioned.",
+        "effect": {
+            "foreign": 0,
+            "civil": 20,
+            "culture": 0,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I use 'ohok', 'ohwow', and/or 'HAHSJW'.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": -20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I need to express my feelings, even if I become irrational.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Spamming is immature, and a waste of time and resources.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": -20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I shout at people who disagree with me.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Keeping peace is an important aspect of maturity and leadership.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": -20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I make logos.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "All I want is peace, and for brother to not fight against brother.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": -20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I post childish, irrelevant memes.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I have exceptional judgement and good social skills.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": -20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I must always be right, even if I blow up half of TMAFE trying to prove myself right.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 20,
+            "social": 0,
+            "economy": 0
+        }
+    },
+    {
+        "question": "It is very important to maintain law and order.",
+        "effect": {
+            "foreign": 0,
+            "civil": 5,
+            "culture": 0,
+            "social": -20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I support same-sex marriage and/or civil unions.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Abortion is murder.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": -20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Racism is wrong.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I do not use or support the common use of pronouns.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": -20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "There are more than two genders.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Transgenderism is foolish, as you cannot change your gender.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": -20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Love is love.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "I am a very religious person.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": -20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "Women should have the same opportunities as men.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 20,
+            "economy": 0
+        }
+    },
+    {
+        "question": "The freer the market, the freer the people.",
+        "effect": {
+            "foreign": 0,
+            "civil": -10,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    },
+    {
+        "question": "I support universal basic income.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 20
+        }
+    },
+    {
+        "question": "Capitalism promotes competition and work incentive.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    },
+    {
+        "question": "The means of production, distribution, and exchange should be owned or regulated by the community as a whole.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 20
+        }
+    },
+    {
+        "question": "1800's-style Capitalism no longer exists, and Capitalism is much fairer now.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    },
+    {
+        "question": "Super-monopolies should never be allowed to form.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 20
+        }
+    },
+    {
+        "question": "The market should determine the prices, not the government.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    },
+    {
+        "question": "We need to raise taxes on the wealthy to better provide for the poor.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": 20
+        }
+    },
+    {
+        "question": "The budget should be balanced, and the debt should never get out of control.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    },
+    {
+        "question": "We should tax everything in TMAFE.",
+        "effect": {
+            "foreign": 0,
+            "civil": 0,
+            "culture": 0,
+            "social": 0,
+            "economy": -20
+        }
+    }
+];
